9b7665e5783e63f8c6c734d0823da197e21ff561
22P31A0444 <119720337+22P31A0444@users.noreply.github.com>
2023-09-14 15:52:28 +0530

    n = int(input()) p=0 g=0 s = n*n while(n!=0):     d=n%10     p=p*10+d     n=n//10 k=p*p while(k!=0):     l=k%10     g=g*10+l     k=k//10 if(g==s):     print("True") else:     print("False")

--- Adam_number.py
+++ Adam_number.py
@@ -1,12 +1,12 @@
 n = int(input())
-s=n*n
 p=0
 g=0
+s = n*n
 while(n!=0):
-    d = n%10
-    p = p*10+d
-    n = n//10
-k = p*p
+    d=n%10
+    p=p*10+d
+    n=n//10
+k=p*p
 while(k!=0):
     l=k%10
     g=g*10+l
